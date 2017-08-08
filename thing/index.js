158ae80efcaf487429c98eebeff28b48eca9e878
James Sturtevant <jsturtevant@gmail.com>
2017-08-08 11:03:52 -0400

    call context.done()

--- thing/index.js
+++ thing/index.js
@@ -1,19 +1,22 @@
 module.exports = function(context, request) {
   const rpn = require('request-promise-native');
   let things = [];
-  return Promise.all([rpn('https://swapi.co/api/planets'), rpn('https://swapi.co/api/people')])
+  
+  Promise.all([rpn('https://swapi.co/api/planets'), rpn('https://swapi.co/api/people')])
     .then(thingArrays => {
       let planetsResponse = JSON.parse(thingArrays[0]);
       let peopleResponse = JSON.parse(thingArrays[1]);
       let planets = planetsResponse.results.map(planet => planet.name);
       let people = peopleResponse.results.map(people => people.name);
       things = [].concat(planets, people);
-      let response = { body: things };
-      return response;
+      context.res = { body: things };
+      context.done();
     })
     .catch(err => {
       context.log('something went wrong');
       context.log.error(err);
-      return { err };
+      context.res = {status:500, body:err}
+      context.done();
     });
+  
 };
